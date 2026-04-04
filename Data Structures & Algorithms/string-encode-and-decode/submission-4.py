0790830f432246f675c25dee4e031dfbd9a388bf
Aliza Haider <alizahayder786@gmail.com>
2026-04-04 22:11:17 +0500

    Update submission-4.py

--- Data Structures & Algorithms/string-encode-and-decode/submission-4.py
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.py
@@ -2,7 +2,8 @@ class Solution:
 
     def encode(self, strs: List[str]) -> str:
         encoded_string = ""
-        
+
+        # delimeter: length of each string + #
         for string in strs:
             encoded_string += str(len(string)) + "#" + string
         
@@ -11,12 +12,17 @@ class Solution:
     def decode(self, s: str) -> List[str]:
         strs, i = [], 0
 
+        # iterate over each word
         while i < len(s):
             j = i
+            # increment j while we still are at integer portion at beginning of #
             while s[j] != "#":
                 j += 1
+            # length of string : integer before #
             length = int(s[i:j])
+            # append word to list starting from one position next to # till the length of word
             strs.append(s[j + 1 : j + 1 + length])
+            # i : starting index of next word
             i = j + 1 + length
         
         return strs
